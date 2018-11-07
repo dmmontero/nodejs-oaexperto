434ebd95c3a394045802e126f5c6d164e3a949f9
dmmontero <danny.montero@gmail.com>
2018-11-07 18:15:01 -0500

    Funciones de flecha

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,10 @@
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+let sumar = (a, b) => a + b;
+
+console.log(sumar(10, 20));
+
+let saludar = nombre => `Hola ${nombre}`;
+console.log(saludar('Jose Turuma'));
