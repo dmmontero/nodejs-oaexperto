df17a1ad0c8fe376ccd1d121414e8ba141bb0e90
dmmontero <danny.montero@gmail.com>
2018-11-27 22:07:55 -0500

    tareas

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,14 +1,23 @@
 const argv = require("./config/yargs").argv;
+const porHacer = require('./por-hacer/por-hacer');
+var colors = require('colors');
 
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
-        console.log('Listar tareas por hacer');
+        let listado = porHacer.getListado();
+        for (let tarea of listado) {
+            console.log("=================Tareas por hacer==========================".rainbow);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log("===========================================================".rainbow);
+        }
         break;
     case 'actualizar':
         console.log('Actualiza una tarea por hacer');
